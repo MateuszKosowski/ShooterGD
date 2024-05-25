2882f768b2aac3548bf83ba86d04a6453de5497e
Mateusz Kosowski <mateuszkosowski03@gmail.com>
2024-05-26 01:19:48 +0200

    Add different behavior for laser

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -7,11 +7,14 @@ signal grenade(pos, direction)
 var can_laser: bool = true
 var can_grenade: bool = true
 
+@export var max_speed: int = 400
+var speed: int = max_speed
+
 func _process(_delta):
 	
 	#Input for movement
 	var direction = Input.get_vector("left", "right", "up", "down")
-	velocity = direction * 400
+	velocity = direction * speed
 	move_and_slide()
 	
 	#Rotate player
--- scenes/projectiles/laser.gd
+++ scenes/projectiles/laser.gd
@@ -3,5 +3,16 @@ extends Area2D
 @export var speed: int = 1500
 var direction: Vector2 = Vector2.UP
 
+func _ready():
+		$SelfDestroy.start()
+
 func _process(delta):
 	position += direction * speed * delta
+
+func _on_body_entered(body):
+	if "hit" in body:
+		body.hit()
+	queue_free()
+	
+func _on_self_destroy_timeout():
+	queue_free()
