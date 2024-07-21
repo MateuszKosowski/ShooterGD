e17ece6c34b5de62bab9febd6be8326643f7276e
Mateusz Kosowski <mateuszkosowski03@gmail.com>
2024-07-21 21:26:25 +0200

    Zoom in/out repaired - toilet layer

--- scenes/houses/house.gd
+++ scenes/houses/house.gd
@@ -1,9 +1,13 @@
 extends Area2D
+
+# Create signals
 signal player_entered
 signal player_exited
 
+# Emit signals
 func _on_body_entered(_body):
 	player_entered.emit()
 	
 func _on_body_exited(_body):
 	player_exited.emit()
+
