9d77ac9ebd48b4cfc91d7c5e29173d9b146f5d04
Mateusz Kosowski <mateuszkosowski03@gmail.com>
2024-09-10 01:29:15 +0200

    Add droping items from toilet

--- scenes/container/toilet.gd
+++ scenes/container/toilet.gd
@@ -1,4 +1,15 @@
 extends ItemContainer
 
 func hit():
-	print('toilet')
+	if not opened:
+		# Hide lid after hit
+		$LidSprite.hide()
+		
+		#Get random Mark2D as a spawnpoint for item
+		var pos = $SpawnPositions/Marker2D.global_position
+	
+		#Emit signal with pos and current_direction (variable from parent node - item_container)
+		open.emit(pos, current_direction)
+			
+		opened = true
+
--- scenes/items/item.gd
+++ scenes/items/item.gd
@@ -1,31 +1,36 @@
 extends Area2D
 
+# Variable declarations/definitions
 var rotation_speed: int = 4
 var ava_options = ['laser', 'laser', 'laser', 'grenade', 'health']
-var type = ava_options[randi()%len(ava_options)]
-
 var direction: Vector2
 var distance: int = randi_range(300,100)
 
+#Randomize type from array
+var type = ava_options[randi()%len(ava_options)]
+
 func _ready():
 	
+	# Depending on type change the Sprite2D color
 	if type == 'laser':
-		$Sprite2D.modulate = Color(0, 0.7, 1)
-		
+		$Sprite2D.modulate = Color(0, 0.7, 1)	
 	if type == 'grenade':
-		$Sprite2D.modulate = Color(1, 0.2, 0.18)
-		
+		$Sprite2D.modulate = Color(1, 0.2, 0.18)	
 	if type == 'health':
 		$Sprite2D.modulate = Color(0.39, 1, 0.35)
-		
+	
+	#Tween to move and scale spawned items	
 	var target_pos = position + direction * distance
-	var movement_tween = create_tween()
-	movement_tween.tween_property(self, "position", target_pos, 0.5)
-		
+	var tween = create_tween()
+	tween.set_parallel(true)
+	tween.tween_property(self, "position", target_pos, 0.5)
+	tween.tween_property(self, "scale", Vector2(1,1), 0.3).from(Vector2(0,0))
+
+# Rotate the item every single frame		
 func _process(delta):
 	rotation += rotation_speed * delta
 
-
+# When some body(player) collide with item add stats to him and delete item
 func _on_body_entered(body):
 	body.add_item(type)
 	queue_free()
