c471fd6aea90a27b3b40fbee5b6903fcc0843dca
Mateusz Kosowski <mateuszkosowski03@gmail.com>
2024-09-15 17:29:51 +0200

    Add Scout health

--- scenes/enemies/scout.gd
+++ scenes/enemies/scout.gd
@@ -2,12 +2,19 @@ extends CharacterBody2D
 
 var player_nearby: bool = false
 var can_laser: bool = true
+var can_get_hit: bool = true
 var one_gun_use: bool = true
+var health: int = 100
 
 signal laser(pos, direction)
 
 func hit():
-	print("Scout was hit")
+	if can_get_hit:
+		health -= 20
+		can_get_hit = false
+		$Timers/HitTImer.start()
+		if health <= 0:
+			queue_free()
 
 func _process(_delta):
 	if player_nearby:
@@ -19,7 +26,7 @@ func _process(_delta):
 			var direction: Vector2 = (Globals.player_pos - position).normalized()
 			laser.emit(pos, direction)
 			can_laser = false
-			$LaserCooldown.start()
+			$Timers/LaserCooldown.start()
 
 func _on_attack_area_body_entered(_body):
 	player_nearby = true
@@ -27,5 +34,8 @@ func _on_attack_area_body_entered(_body):
 func _on_attack_area_body_exited(_body):
 	player_nearby = false
 
-func _on_laser_cooldown_timeout():
+func _on_hit_timer_timeout():
+	can_get_hit = true
+
+func _on_laser_timer_timeout():
 	can_laser = true
