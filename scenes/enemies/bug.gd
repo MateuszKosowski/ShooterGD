fdbb685490d7696bdc256d33d72d91875a291b53
Mateusz Kosowski <mateuszkosowski03@gmail.com>
2024-10-09 18:02:32 +0200

    Add particles for Bug

--- scenes/enemies/bug.gd
+++ scenes/enemies/bug.gd
@@ -45,8 +45,12 @@ func hit():
 		vulnerable = false
 		$Timer/HitTimer.start()
 		health -= 10
+		$AnimatedSprite2D.material.set_shader_parameter("progress", 1)
+		$Particles/HitParticles.emitting = true
 	if health <= 0:
+		await get_tree().create_timer(0.5).timeout
 		queue_free()
 
 func _on_hit_timer_timeout():
 	vulnerable = true
+	$AnimatedSprite2D.material.set_shader_parameter("progress", 0)
