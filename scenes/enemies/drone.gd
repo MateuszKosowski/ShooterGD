29fce4702046190b7a7a7f744914c654bfa631df
Mateusz Kosowski <mateuszkosowski03@gmail.com>
2024-10-19 23:26:09 +0200

    Add drone

--- scenes/enemies/drone.gd
+++ scenes/enemies/drone.gd
@@ -1,13 +1,33 @@
 extends CharacterBody2D
 
-func _process(_delta):
-	#direction
-	var direction = Vector2(1,0)
-	#velocity
-	velocity = direction *300
-	#move and slide
-	move_and_slide()
+var active: bool = false
+var vulnerable: bool = true
+var health: int = 50
+var speed: int = 400
+
+func _process(delta):
+	if active:
+		look_at(Globals.player_pos)
+		var direction = (Globals.player_pos - position).normalized()
+		velocity = direction * speed
+		var collision = move_and_collide(velocity * delta)
+		if collision:
+			$AnimationPlayer.play("explosion")
+		
+func _ready():
+	$Explosion.hide()
+	$Sprite2D.show()
 	
 func hit():
-	print('Take dmg...!')
+	if vulnerable:
+		health -= 10
+		vulnerable = false
+		$HitTimer.start()
+	if health <= 0:
+		$AnimationPlayer.play("explosion")
 	
+func _on_notice_area_body_entered(_body):
+	active = true
+
+func _on_hit_timer_timeout():
+	vulnerable = true
